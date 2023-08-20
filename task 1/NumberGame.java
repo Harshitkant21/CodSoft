1eec98dd7caac9463c8c9941f37654d67bf0a274
Harshitkant21 <harshit0576.be21@chitkara.edu.in>
2023-08-20 17:23:13 +0530

    Updated

--- task 1/NumberGame.java
+++ task 1/NumberGame.java
@@ -1,27 +1,27 @@
-package com.codSoft;
-
 import java.util.Scanner;
 import java.util.Random;
 
 public class NumberGame {
-    private static final int Max_tries = 10;
+    private static final int Max_tries = 10; // Maximum number of tries allowed
     
-    //Method to play the game
+    // Method to play the game
     public static void guess(int num, int randomNum) {
-        int turns = 0;
+        int turns = 0; // Counter for the number of tries
         while (num != randomNum && turns < Max_tries) {
+            // Provide hints to the user based on their guess
             if (num < randomNum) {
                 System.out.println("Your guess is lower than the expected number. Think of a bit higher value.");
             } else {
                 System.out.println("Your guess is higher than the expected number. Think of a bit lower value.");
             }
-            turns++;
+            turns++; // Increment the turn counter
             System.out.print("Enter your next guess: ");
-            num = new Scanner(System.in).nextInt();
+            num = new Scanner(System.in).nextInt(); // Read the next guess from the user
         }
 
+        // Display the result of the game based on the final guess
         if (num == randomNum) {
-            System.out.println("Congratulations!! You got "+(100-(turns*10))+" points.");
+            System.out.println("Congratulations!! You got " + (100 - (turns * 10)) + " points.");
         } else {
             System.out.println("Sorry!! You Lost. Correct number is " + randomNum);
             System.out.println("Try Again");
@@ -29,23 +29,29 @@ public class NumberGame {
     }
 
     public static void main(String[] args) {
-    	Scanner in = new Scanner(System.in);
-    	do {
-	        System.out.println("Welcome to Number Guessing Game!!");
-	        System.out.println("It is point based game maximum points will be 100.");
-	        System.out.println("You have only 10 chances to win the Game. Best Of Luck!!!!");
-	        Random rand = new Random();
-	        int number = rand.nextInt(100) + 1; 
-	
-	        System.out.print("Hey, I am guessing a number from range 1-100. Can you guess that number: ");
-	        int userGuess = in.nextInt();
-	
-	        if (userGuess < 0 || userGuess > 100) {
-	            System.out.println("Invalid! Enter a valid number in range 1-100.");
-	        } else {
-	            guess(userGuess, number);
-	        }
-	        System.out.println("Do you want to play again? (yes/no): ");
-    	}while("yes".equalsIgnoreCase((in.next())));        
+        Scanner in = new Scanner(System.in);
+        do {
+            // Welcome message and game instructions
+            System.out.println("Welcome to Number Guessing Game!!");
+            System.out.println("It is a point-based game. The maximum points you can get is 100.");
+            System.out.println("You have only 10 chances to win the game. Best of luck!");
+            
+            Random rand = new Random();
+            int number = rand.nextInt(100) + 1; // Generate a random number between 1 and 100
+            
+            System.out.print("Hey, I am guessing a number from range 1-100. Can you guess that number: ");
+            int userGuess = in.nextInt(); // Read the user's initial guess
+            
+            // Check if the user's guess is out of range and provide instructions
+            if (userGuess < 1 || userGuess > 100) {
+                System.out.println("Invalid! Enter a valid number in the range 1-100.");
+            } else {
+                guess(userGuess, number); // Call the guess method to play the game
+            }
+            
+            System.out.print("Do you want to play again? (yes/no): ");
+        } while ("yes".equalsIgnoreCase(in.next())); // Continue the loop if the user wants to play again
+        
+        in.close(); // Close the scanner to release resources
     }
 }
---
+++ task 2/StudentGrade.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+public class StudentGrade {
+    public static void main(String[] args) {
+        // Create a new Scanner object to read input from the console
+        Scanner in = new Scanner(System.in);
+        
+        // Prompt the user to enter the student's name and store it in a variable
+        System.out.print("Please enter Student Name: ");
+        String sName = in.nextLine();
+
+        // Declare a variable to store the number of subjects
+        int subNum;
+        do {
+            // Prompt the user to enter the number of subjects and validate it
+            System.out.print("Enter number of subjects: ");
+            subNum = in.nextInt();
+            if (subNum <= 0) {
+                System.out.println("Please enter a valid number of subjects.");
+            }
+        } while (subNum <= 0);
+
+        // Prompt the user to enter subjects and their corresponding marks
+        System.out.println("Enter subjects and marks: ");
+        // Create a LinkedHashMap to store subjects and their marks
+        LinkedHashMap<String, Integer> marks = new LinkedHashMap<>();
+        int subtotal = 0;
+
+        // Iterate through each subject and its corresponding mark
+        for (int i = 1; i <= subNum; i++) {
+            // Prompt the user to enter the subject name and store it
+            System.out.print("Enter " + i + " subject: ");
+            String subject = in.next();
+            
+            int val;
+            // Validate the input for marks and handle exceptions
+            do {
+                System.out.print("Enter marks in " + subject + ": ");
+                try {
+                    val = in.nextInt();
+                    if (val < 0 || val > 100) {
+                        System.out.println("Please enter valid marks between 0 and 100.");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Please enter a numeric value.");
+                    // Clear the input buffer and set val to trigger re-entry
+                    in.nextLine();
+                    val = -1;
+                }
+            } while (val < 0 || val > 100);
+
+            // Add the subject and mark to the LinkedHashMap
+            marks.put(subject, val);
+            // Update the subtotal with the current mark
+            subtotal += val;
+        }
+
+        // Display the entered subjects and their marks
+        System.out.println();
+        System.out.println(marks);
+        // Calculate and display the total marks obtained by the student
+        System.out.println("Total Marks obtained by " + sName.toUpperCase() + " in " + marks.size() +
+                " subjects out of " + marks.size() * 100 + " is: " + subtotal);
+        // Calculate the average percentage
+        double avg = (double) subtotal / marks.size();
+        // Display the average percentage with proper formatting
+        System.out.printf("Average percentage of all %d subjects is: %.2f%%%n", marks.size(), avg);
+        
+        // Determine the grade based on the average and display it
+        String grade = switch ((int) avg / 10) {
+            case 9 -> "A";
+            case 8 -> "B";
+            case 7 -> "C";
+            case 6 -> "D";
+            case 5 -> "E";
+            default -> "Fail";
+        };
+        
+        // Display the final grade
+        System.out.println();
+        System.out.print("Grade Scored by " + sName.toUpperCase() + " is " + grade);
+
+        // Close the Scanner to release resources
+        in.close();
+    }
+}
